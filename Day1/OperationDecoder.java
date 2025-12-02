585117ece1256ba45890b52292306705932acc71
m-bernardin <mjrb4mjrb@gmail.com>
2025-12-02 13:51:54 -0500

    pleas woeke fuvck,f

--- Day1/OperationDecoder.java
+++ Day1/OperationDecoder.java
@@ -14,49 +14,33 @@ public class OperationDecoder{
     }
 
     public void turnLeft(int amnt){
-        for(int i=0;i<amnt;++i){
-            if(pointer==0){
-                pointer=99;
-            }
-            else{
-                --pointer;
-            }
-        }
-        if(pointer==0){
-            ++count;
-        }
+        int turnAmnt=amnt%100;
+        pointer-=turnAmnt;
+        if(pointer==0)++count;
     }
 
     public void turnRight(int amnt){
-        for(int i=0;i<amnt;++i){
-            if(pointer==99){
-                pointer=0;
-            }
-            else{
-                ++pointer;
-            }
-        }
-        if(pointer==0){
-            ++count;
-        }
+        int turnAmnt=amnt%100;
+        pointer+=turnAmnt;
+        if(pointer==0)++count;
     }
 
     public void decodeInstructions(){ 
         for(int i=0;i<operations.size();++i){
-            String operation=operations.get(0);
+            String operation=operations.get(i);
             //System.out.println(operation);
-            //System.out.println(operation.substring(1));
+            System.out.println(operation.substring(1));
             int amnt=Integer.parseInt(operation.substring(1));
             char direction=operation.charAt(0);
-            //System.out.println(direction);
-            //System.out.println(amnt);
-
+            System.out.println(direction);
+            System.out.println(amnt);
             if(direction=='L'){
                 turnLeft(amnt);
             }
             else if(direction=='R'){
                 turnRight(amnt);
             }
+            
             System.out.println(pointer);
         }
         System.out.println("The code is: "+count);
