fe23f235c3312f07cd63c9266809aba0b7f8deb8
m-bernardin <mjrb4mjrb@gmail.com>
2025-12-05 23:52:00 -0500

    i cant use bash :(
    not everything pushed

---
+++ Day3/Driver3.java
@@ -0,0 +1,7 @@
+public class Driver3{
+	public static void main(String[] args)
+	{
+		@SuppressWarnings("unused")
+		Decoder3 decoder = new Decoder3();
+	}
+}
---
+++ duskBringer.sh
@@ -0,0 +1,4 @@
+echo Attempting to execute: Day$1
+cd Day$1
+javac *.java
+java Driver$1
